5d6fba738dc0306cbb39d2ee70305911b6ef2b0b
Ajay Kumar Nagar <anagarjnv889@gmail.com>
2021-04-16 15:40:30 +0530

    Final

--- 3_Implementation/test/unit_test.c
+++ 3_Implementation/test/unit_test.c
@@ -11,91 +11,29 @@
 #include "unity.h"
 #include "head.h"
 #define PROJECT_NAME    "Calculator"
-ajay testcal={10,5};
-ajay1 testo={4};
-ajay testcal1={-3,-2} /* negative number input  */,testdiv0={3,0};
-ajay1 testf1={0};
-ajay1 testf2={-5}; /* negative number input  */
-/* Prototypes for all the test functions */
-void test_addition(void);
-void test_subtraction(void);
-void test_multiplication(void);
-void test_division(void);
-  
-void test_factorial(void);
-void test_zero_factorial(void);
-void test_negative_factorial(void);
-void test_division_byZero(void);
-void test_division_byZero(void);
- 
-/* Required by the unity test framework */
 void setUp(){}
-/* Required by the unity test framework */
+/**
+ * @brief 
+ * 
+ */
 void tearDown(){}
+/**
+ * @brief 
+ * 
+ */
+ void test_sourcecode(void){
 
-/* Start of the application test */
-int main()
+    int a = 65;
+    int b = 2;
+    
+    TEST_ASSERT_EQUAL(130,addition(a, b));
+
+}
+int main(void)
 {
-/* Initiate the Unity Test Framework */
-  UNITY_BEGIN();
-
-/* Run Test functions */
-  RUN_TEST(test_addition);
-  RUN_TEST(test_subtraction);
-  RUN_TEST(test_multiplication);
-  RUN_TEST(test_division);
-  RUN_TEST(test_division_byZero);
-  RUN_TEST(test_power);
-   
-  RUN_TEST(test_factorial);
-  RUN_TEST(test_zero_factorial);
-  RUN_TEST(test_negative_factorial);
-
-  /* Close the Unity Test Framework */
-  return UNITY_END();
-}
-
-/* all test functions */ 
-void test_addition(void) {
-  TEST_ASSERT_EQUAL(15,addition(&testcal));
-   TEST_ASSERT_EQUAL(-5,addition(&testcal1)); /* negative number input case  */
-   }
-void test_substraction(void) {
-  TEST_ASSERT_EQUAL(5,subtraction(&testcal));
-  TEST_ASSERT_EQUAL(-1,subtraction(&testcal1)); /* negative number input case  */
-}
-void test_multiplication(void) {
-  TEST_ASSERT_EQUAL(50,multiplication(&testcal));
-  TEST_ASSERT_EQUAL(6,multiplication(&testcal1)); /* negative number input case  */
-}
-void test_division(void) {
-  TEST_ASSERT_EQUAL(2,division(&testcal));
-  TEST_ASSERT_EQUAL(1,division(&testcal1)); /* negative number input case  */
-}
-/*division when divisor is 0 */
-void test_division_byZero(void){
-   TEST_ASSERT_EQUAL(-1, division(&testdiv0));
-}
-void test_power(void) {
-  TEST_ASSERT_EQUAL(100000,power(&testcal));
-   TEST_ASSERT_EQUAL(-0.111,power(&testcal1)); /* negative number input case  */
-}
-
-void test_factorial(void) {
-  TEST_ASSERT_EQUAL(24,factorial(&testo));
-  TEST_ASSERT_EQUAL(-1,factorial(&testf2)); /* negative number input case  */
-}
-
-/*factorial of 0*/
-void test_zero_factorial(void)
-{
-  TEST_ASSERT_EQUAL(1, factorial(&testf1));
-}
-
-/*factorial of negative numbers */
-void test_negative_factorial(void)
-{
-  TEST_ASSERT_EQUAL(-1, factorial(&testf2));
+UNITY_BEGIN();
+RUN_TEST(test_sourcecode); 
+return UNITY_END();
 }
 
  
