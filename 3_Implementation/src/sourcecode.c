b2cb24200dba0ff37bc4fee7c2112b5bd7498d29
Ajay Kumar Nagar <anagarjnv889@gmail.com>
2021-04-16 15:25:18 +0530

    Final Commit

--- 3_Implementation/src/sourcecode.c
+++ 3_Implementation/src/sourcecode.c
@@ -16,147 +16,119 @@
 /**
  * @brief 
  * 
- * @param nagar 
+ * @param a 
+ * @param b 
  * @return int 
  */
-int addition(ajay *nagar)
+int addition(int a,int b)
 {
-    int ans= nagar->a+nagar->b;
+    int ans= a+b;
     return ans;
 }
 /**
  * @brief 
  * 
- * @param nagar 
+ * @param a 
+ * @param b 
  * @return int 
  */
-int subtraction(ajay *nagar)
+int subtraction(int a,int b)
 {
-    int ans=(nagar->a)-(nagar->b);
+    int ans= a-b;
     return ans;
 }
 /**
  * @brief 
  * 
- * @param nagar 
+ * @param a 
+ * @param b 
  * @return int 
  */
-int multiplication(ajay *nagar)
+int multiplication(int a,int b)
 {
-    int ans=(nagar->a)*(nagar->b);
+    int ans= a*b;
     return ans;
 }
 /**
  * @brief 
  * 
- * @param nagar 
+ * @param a 
+ * @param b 
  * @return int 
  */
-int subtraction(ajay *nagar)
+int dividion(int a,int b)
 {
-    int ans=nagar->a - nagar->b;
-    return ans;
-}
-/**
- * @brief 
- * 
- * @param nagar 
- * @return int 
- */
-int division(ajay *nagar)
-{
-    if(nagar->b!=0)
+    if(b==0)
     {
-        int ans=(nagar->a)/(nagar->b);
-    return ans;
-
-    }
-    else{
-        printf("\nEnter a valid input!!!! Divisor can not be zero\n");
         return -1;
     }
+    int ans= a/b;
+    return ans;
+}
+/**
+ * @brief 
+ * 
+ * @param a 
+ * @param b 
+ * @return int 
+ */
+int modulus(int a,int b)
+{
+    if(b==0)
+    {
+        return -1;
+    }
+    int ans= a%b;
+    return ans;
+}
+/**
+ * @brief 
+ * 
+ * @param a 
+ * @param b 
+ * @return int 
+ */
+int gcd(int a,int b)
+{
+    int gc=1;
+   for(int i=0;i<=a && i<=b;i++)
+   {
+    if(a%i==0 && b%i==0)
+    {
+      gc=i;
+    }
+   }
+   return gc;
      
 }
 /**
  * @brief 
  * 
- * @param nagar 
+ * @param a 
  * @return int 
  */
-int modulus(ajay *nagar)
-{
-    if(nagar->b!=0)
-    {
-        int ans=(nagar->a)%(nagar->b);
-    return ans;
-
-    }
-    else{
-        printf("\nEnter a valid input!!!! Divisor can not be zero\n");
-        return -1;
-    }
-}
-/**
- * @brief 
- * 
- * @param nagar 
- * @return int 
- */
-int power(ajay *nagar)
-{
-    int ans=pow(nagar->a,nagar->b);
-    return ans;
-}
-/**
- * @brief 
- * 
- * @param nagar 
- * @return int 
- */
-int gcd(ajay *nagar)
-{
-     int ans=1;
-     for(int i=2;i<=(nagar->a) && i<=(nagar->b);i++)
-     {
-         if(i%(nagar->a)==0 && i%(nagar->b)==0)
-         {
-             ans=1;
-         }
-     }
-     return ans;
-}
-int factorial(ajay1 *nagar1)
-{
-    if(nagar1->n < 0)
-    {
-        printf("\nEnter a positie number \n");
-        return -1;
-    }
-    if(nagar1->n==0)
+int factorial(int a)
+{              
+    if(a==0 || a==1)
     {
         return 1;
     }
-    long long int f=1;
-    for(int i=1;i<= nagar1->n;i++)
-    {
-        f=f*i;
-    }
-    return f;
+    return factorial(a-1)*a;
 }
 /**
  * @brief 
  * 
- * @param nagar1 
+ * @param a 
  * @return int 
  */
-int sumofdigit(ajay1 *nagar1)
+int sumofdigit(int a)
 {
-    int ans=0;
-    while((nagar1->n) > 0)
+    int sum=0;
+    while(a>0)
     {
-        int rem=(nagar1->n)%10;
-        ans+=rem;
-    nagar1->n=(nagar1->n)/10;
+        int rem=a%10;
+        sum+=rem;
+        a=a/10;
     }
-    return ans;
+    return sum;
 }
